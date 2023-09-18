90300c2a90efb07974da417472c2be4ebf38cfb8
nikostrauch <113108195+nikostrauch@users.noreply.github.com>
2023-09-18 19:33:35 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
-test
+<html>
+  <body>
+    <form>
+      <p>from</p>
+      <input type="email" id="from">
+      <p>name</p>
+      <input type="text" id="name">
+      <p>to</p>
+      <input type="email" id="to">
+      <p>subject</p>
+      <input type="text" id="subject">
+      <p>text</p>
+      <textarea id="text"></textarea>
+  </body>
+</html>
